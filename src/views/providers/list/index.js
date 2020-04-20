95647cb3c0c01e87ff2d4ea245ecd84af380d3fd
Rafa Moreno <rmh1516@gmail.com>
2020-04-20 16:48:37 +0200

    fix filter title

--- src/views/providers/list/index.js
+++ src/views/providers/list/index.js
@@ -54,7 +54,7 @@ const ProvidersList = ({
                 </th>
                 <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                   <Filter
-                    title="Ciudad"
+                    title="Población"
                     value={filters.city}
                     onChange={event =>
                       updateFilters({ city: event.target.value })
